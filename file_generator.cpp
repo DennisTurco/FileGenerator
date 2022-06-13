66bea6c559d35b152847da91ab0f8d6551f12058
Dennis Turco <57963761+Mr-Denno@users.noreply.github.com>
2022-06-13 21:44:41 +0200

    Add files via upload

--- file_generator.cpp
+++ file_generator.cpp
@@ -4,69 +4,36 @@
 #include <cstdlib>
 #include <time.h>
 
-using namespace std;
+void ordinatore_su_file(int *array, const int &dim, const char *nome, const std::string &delimitatore){
+	std::ofstream file;
+	char *nome2 = new char[32 + strlen("_ordinato")]; // il nuovo file avra' la lunghezza del primo piu' la lunghezza della stringa "_ordinato".
+	char *estensione = new char[10];
+	int index = 0;
 
-void generatore (ostream&, char[]);
-void ordinatore (const ostream&, int[], int, char[]);
-
-int main (){	
-	ofstream f1;	
-	char string[32];
-	cout<<"\tCreated By Dennis Turco\n";	
-	
-	cout<<"Inserire nome file: ";
-	cin.getline(string, 32);
-	f1.open(string);
-	
-	
-	system("cls");
-	generatore(f1, string);
-	
-	/*srand(time(NULL)); // Inizializza generatore di numeri pseudo-casuali
-	int contatore = 0;
-	int contatore2 = 0;
-	int numero;
-	int numeri_da_generare;
-	int inizio, fine;	
-	
-	cout<<"Quantita' di numeri da generare: ";
-	cin>>numeri_da_generare;
-	cout<<"A partire da: ";
-	cin>>inizio;
-	cout<<"Fino a: ";
-	cin>>fine;	
-	f1<<"Numeri generati: "<<numeri_da_generare<<endl;
-	while (contatore<numeri_da_generare){
-		numero = rand();
-		cout<<numero<<endl;
-		if (numero>=inizio && numero<=fine){
-			f1<<numero<<endl;
-			contatore++;
-		}
-	}
-	*/
-			
-	f1.close();
-	system("pause");
-	return 0;	
-}
-
-void ordinatore(const ostream& f1, int array[], int dim, char nome[]){
-	ofstream f2;
-	char nome2[32];
 	for (int i=0; i<32; i++){
-		if (nome[i] != '.')
+		if (nome[i] != '.'){
 			nome2[i] = nome[i];
-		else break;
+			nome2[i+1] = '\0';
+		}
+		else {
+			//trovo l'estensione
+			for (int j=i; j<10; j++){
+				estensione[index] = nome[j];
+				estensione[index+1] = '\0';
+				index++;
+			}
+		}	
 	}
-	strcat (nome2, "_ordinato.txt");
-	f2.open(nome2);
+
+	strcat(nome2, "_ordinato");
+	if (index != 0) strcat(nome2, estensione); // concateno solo se il nome ha un estensione
+	file.open(nome2);
 	
-	//ORDINAMENTO DEI VALORI NEL FILE	
+	//ORDINAMENTO DEI VALORI NEL FILE (ordinamento poco efficiente ma non mi interessa fare poche letture dell'array)
 	int temp;
 	for (int i=0; i<dim; i++){
 		for (int j=i+1; j<dim; j++){
-			if (array[i]>array[j]){
+			if (array[i] > array[j]){
 				temp = array[i];
 				array[i] = array[j];
 				array[j] = temp;
@@ -75,76 +42,89 @@ void ordinatore(const ostream& f1, int array[], int dim, char nome[]){
 	}
 	
 	//STAMPA DEL NUOVO FILE ORDINATO
-	f2<<"Numeri generati: "<<dim<<endl;
+	file<<"Numeri generati: "<<dim<<std::endl;
 	for (int i=0; i<dim; i++){
-			f2<<array[i]<<endl;
+		file<<array[i];
+		if(i != dim-1) file<<delimitatore; // non voglio che mi aggiunga il delimitatore all'ultimo elemento
 	}
 		
-	cout<<"Ordinamento completato!";
-	f2.close();
-	return;
+	file.close();
 }
 
-void generatore(ostream& f1, char nome[]){
+int *generatore_su_file(std::ostream& file, const int &numeri_da_generare, const int &start, const int &end, const std::string &delimitatore){
 	srand(time(NULL)); // Inizializza generatore di numeri pseudo-casuali
+	
 	int contatore = 0;
-	int contatore2 = 0;
-	int numero;
-	int numeri_da_generare;
-	int inizio, fine;	
+	int random;
+	int *contenitore = new int[numeri_da_generare];
 	
-	cout<<"Quantia' di numeri da generare: ";
-	cin>>numeri_da_generare;
-	cout<<"A partire da: ";
-	cin>>inizio;
-	cout<<"Fino a: ";
-	cin>>fine;	
-	f1<<"Numeri generati: "<<numeri_da_generare<<endl;
-	
-	int contenitore[numeri_da_generare];
-	
+	// generazione randomica
 	while (contatore < numeri_da_generare){
-		numero = rand();
-		if(numero>fine){	
-			for (int i=0; i<10; i++){
-				numero = numero/2;
-				if(numero>=inizio && numero<=fine){
-					f1<<numero<<endl;
-					contenitore[contatore] = numero;
-					contatore++;
-					break;
-				}
-			}	
-		}
-		
-		if (numero>=inizio && numero<=fine){
-			f1<<numero<<endl;
-			contenitore[contatore] = numero;
-			contatore++;
-		}
-		
-		if (numero<inizio){
-			for (int i=0; i<10; i++){
-				numero = numero*2;
-				if(numero>=inizio && numero<=fine){
-					f1<<numero<<endl;
-					contenitore[contatore] = numero;
-					contatore++;
-					break;
-				}
-			}
-		}
-		contatore2++;
+		random = rand() % (end-start+1) + start; //int randNum = rand()%(max-min + 1) + min;
+		file<<random;
+		if(contatore != numeri_da_generare-1) file<<delimitatore; // non voglio che mi aggiunga il delimitatore all'ultimo elemento
+		contenitore[contatore] = random;
+		contatore++;
 	}
 	
-	cout<<"Numero totale di numeri cercati: "<<contatore2; 
-	cout<<"\nStampa terminata!\n";
+	return contenitore;
 	
+}
+
+int main (void) {	
+	std::ofstream f1;	
+	char *nome_file = new char[32];
+	std::cout<<"\tCreated By Dennis Turco\n";	
+	
+	std::cout<<"Inserire nome file: ";
+	std::cin.getline(nome_file, 32);
+	f1.open(nome_file);
+
+
+	// -------------------------------- GENERAZIONE
+	int numeri_da_generare;
+	int inizio, fine;
+	char *delimitatore = new char[5];
+
+	std::cout<<"Quantita' di numeri da generare: ";
+	std::cin>>numeri_da_generare;
+	std::cout<<"A partire da: ";
+	std::cin>>inizio;
+	std::cout<<"Fino a: ";
+	std::cin>>fine;	
+	std::cin.ignore(10000, '\n'); // pulisco il cin
+	std::cout<<"Carattere/i di spaziatura (max 5 caratteri): ";
+	std::cin.getline(delimitatore, 5);
+	f1<<"Numeri generati: "<<numeri_da_generare<<std::endl;
+	
+	system("cls");
+	int *contenitore = generatore_su_file(f1, numeri_da_generare, inizio, fine, delimitatore);
+	std::cout<<"\nStampa terminata!\n";
+
 	char scelta;
-	cout<< ("Vuoi ordinare in modo crescente i valori del file? (s/n): ");
-	cin>>scelta;
-	if (scelta=='s')
-		ordinatore(f1, contenitore, numeri_da_generare, nome);
+	std::cout<< ("Vuoi ordinare in modo crescente i valori del file? (s/n): ");
 	
-	return;
+	do {
+		std::cin>>scelta;
+		if (scelta == 's') {
+			//-------------------------------- ORDINAMENTO
+			ordinatore_su_file(contenitore, numeri_da_generare, nome_file, delimitatore);
+			std::cout<<"Ordinamento completato!\n";
+			break;
+		}
+		else if (scelta == 'n') {
+			break;
+		}
+		else {
+			std::cin.ignore(10000, '\n'); // pulisco il cin
+			std::cout<<"Errore!! Ripetere l'immissione: ";
+		}
+		
+	} while (true);
+			
+	f1.close();
+
+	system("pause");
+
+	return EXIT_SUCCESS;	
 }
